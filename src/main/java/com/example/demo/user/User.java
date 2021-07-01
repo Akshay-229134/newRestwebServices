c7bbecd33cc759a42fde8623f84e51e8c85c5609
akshay229134 <ganeshdeshmukh1612@gmail.com>
2021-07-01 11:06:10 +0530

    new commit

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -1,18 +1,40 @@
 package com.example.demo.user;
 
 import java.util.Date;
+import java.util.List;
 
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Size;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+//import javax.validation.constraints.Past;
+//import javax.validation.constraints.Size;
+@Entity
 public class User {
-	
+	@Id
+	@GeneratedValue
 	private Integer id;
-	@Size(min=2,message="Name should have 2 char")
+	//@Size(min=2,message="Name should have 2 char")
 	private String name;	
-	@Past
+	//@Past
 	private Date birthdate;
 	
+	
+	
+	//As 1 user can have multiple posts,
+	//and 1 post can have 1user only
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	protected User() {
 
 	}
